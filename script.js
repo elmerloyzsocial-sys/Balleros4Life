4c370f7b3612bfbfd05a8724fe77564d77ccc6b0
elmerloyzsocial-sys <elmerloyz.social@gmail.com>
2025-08-27 11:47:10 +1000

    script and standing html added

--- script.js
+++ script.js
@@ -7,11 +7,21 @@ const games = [
   { date: '2025-08-08', opponent: 'Sharks', location: 'Home', result: 'Loss', ourScore: 65, oppScore: 70 }
 ];
 
-const standings = [
-  { team: 'Ballero4Life', wins: 1, losses: 1 },
-  { team: 'Falcons', wins: 2, losses: 0 },
-  { team: 'Eagles', wins: 1, losses: 1 },
-  { team: 'Bulls', wins: 0, losses: 2 }
+const standing = [
+  { name: 'Masagana', record: '8–0', pointDifferential: 125 },
+  { name: 'World Remit', record: '6–2', pointDifferential: 45 },
+  { name: 'Ted Fencing', record: '5–3', pointDifferential: 71 },
+  { name: "Henry's Harvest", record: '5–3', pointDifferential: 54 },
+  { name: 'Hype Signs', record: '5–3', pointDifferential: 18 },
+  { name: 'Laserbond', record: '4–4', pointDifferential: 15 },
+  { name: 'Elegance Eyewear', record: '4–3', pointDifferential: -4 },
+  { name: 'Prime One', record: '4–4', pointDifferential: -5 },
+  { name: 'Poke Bowl', record: '3–4', pointDifferential: -14 },
+  { name: 'RnR Sportswear', record: '3–5', pointDifferential: -22 },
+  { name: 'Simply Fin Wiz', record: '3–5', pointDifferential: -80 },
+  { name: "Angie's Vlog", record: '2–6', pointDifferential: -61 },
+  { name: 'McGrath Mitsubishi', record: '2–6', pointDifferential: -78 },
+  { name: 'Jet1 Plumbing', record: '1–7', pointDifferential: -78 }
 ];
 
 const plays = [
