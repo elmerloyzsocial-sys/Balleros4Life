5ab270febaaf179734e9378ea67dc576c08aa18f
elmerloyzsocial-sys <elmerloyz.social@gmail.com>
2025-09-25 09:21:58 +1000

    Update standings-data.js

--- standings-data.js
+++ standings-data.js
@@ -1,19 +1,19 @@
 // Example standings data structure
 const standing = [
   // Example
-  { name: 'Masagana', record: '9–0', pointDifferential: 127 },
-  { name: 'World Remit', record: '6–3', pointDifferential: 38 },
-  { name: 'Ted Fencing', record: '5–4', pointDifferential: 72 },
-  { name: "Henry's Harvest", record: '5–4', pointDifferential: 32 },
-  { name: 'Hype Signs', record: '6–3', pointDifferential: 27 },
-  { name: 'Laserbond', record: '6–3', pointDifferential: 16 },
-  { name: 'Elegance Eyewear', record: '4–5', pointDifferential: -5 },
-  { name: 'First One', record: '5–4', pointDifferential: -20 },
-  { name: 'Poke Bowl', record: '4–5', pointDifferential: 21 },
-  { name: 'RnR Sportswear', record: '4–5', pointDifferential: -3 },
-  { name: 'Simply Fin Wiz', record: '3–6', pointDifferential: -85 },
-  { name: "Angie's Vlog", record: '2–7', pointDifferential: -50 },
-  { name: 'McGrath Mitsubishi', record: '3–6', pointDifferential: -88 },
-  { name: 'Jet1 Plumbing', record: '1–8', pointDifferential: -82 }
+  { name: 'Masagana', record: '10–0', pointDifferential: 131 },
+  { name: 'World Remit', record: '6–4', pointDifferential: 34 },
+  { name: 'Ted Fencing', record: '5–5', pointDifferential: 29 },
+  { name: "Henry's Harvest", record: '6–4', pointDifferential: 44 },
+  { name: 'Hype Signs', record: '7–3', pointDifferential: 34 },
+  { name: 'Laserbond', record: '6–4', pointDifferential: 9 },
+  { name: 'Elegance Eyewear', record: '4–6', pointDifferential: -17 },
+  { name: 'First One', record: '6–4', pointDifferential: 23 },
+  { name: 'Poke Bowl', record: '5–5', pointDifferential: 35 },
+  { name: 'RnR Sportswear', record: '5–5', pointDifferential: 32 },
+  { name: 'Simply Fin Wiz', record: '3–7', pointDifferential: -120 },
+  { name: "Angie's Vlog", record: '2–8', pointDifferential: -64 },
+  { name: 'McGrath Mitsubishi', record: '3–7', pointDifferential: -95 },
+  { name: 'Jet1 Plumbing', record: '2–8', pointDifferential: -75 }
   // Add all teams up to 14
 ];
